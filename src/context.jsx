efa94826faa4a32dcd30c9524a72db8f6bd35fb1
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-15 10:15:56 +0530

    Create Global Context

---
+++ src/context.jsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useState } from 'react';
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsSidebarOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        closeModal,
+        closeSidebar,
+        openModal,
+        openSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// Custom hooks
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
